19b1db5f79877e25c4153952fec404fb9c79b6e7
Divkov575 <dmitriy@ivkov.net>
2024-08-05 11:18:56 -0400

    'Added toto list'

--- example/basic.rs
+++ example/basic.rs
@@ -1,7 +1,24 @@
 extern crate tabprint;
 use tabprint::Table;
 
-#[allow(dead_code)]
+/*
+	Following main function will print :
+	+---------+------+---------+
+	| ABC     | DEFG | HIJKLMN |
+	+---------+------+---------+
+	| foobar  | bar  | foo     |
+	+---------+------+---------+
+	| foobar2 | bar2 | foo2    |
+	+---------+------+---------+
+	Modified : 
+	+---------+------+---------+
+	| ABC     | DEFG | HIJKLMN |
+	+---------+------+---------+
+	| foobar  | bar  | foo     |
+	+---------+------+---------+
+	| foobar2 | bar2 | new_foo |
+	+---------+------+---------+
+*/
 fn main() {
     let mut table = Table::new(vec!["ABC".to_string(), "DEFG".to_string(), "HIJKLMN".to_string()]);
     table.add_row(vec!["foobar".to_string(), "bar".to_string(), "foo".to_string()]).unwrap();
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! A formatted and aligned table printer written in rust
+use std::io::{stdout, Write, Error};
+
+/// A Struct representing a printable table
 pub struct Table {
 	num_cols: usize,
 	titles: Vec<String>,
@@ -5,6 +9,7 @@ pub struct Table {
 }
 
 impl Table {
+	/// Create a new table with the number of columns equals to the length of `titles`
 	pub fn new(titles: Vec<String>) -> Table {
 		let n = titles.len();
 		return Table {
@@ -14,22 +19,28 @@ impl Table {
 		};
 	}
 	
+	/// Get the number of column
 	pub fn get_column_num(&self) -> usize {
 		return self.num_cols;
 	}
 	
+	/// Get the number of rows
 	pub fn get_rows_number(&self) -> usize {
 		return self.rows.len();
 	}
 	
+	/// Get a mutable reference to a row
 	pub fn get_mut_row(&mut self, row: usize) -> &mut Vec<String> {
 		return &mut self.rows[row];
 	}
 	
+	/// Get an immutable reference to a row
 	pub fn get_row(&self, row: usize) -> &Vec<String> {
 		return &self.rows[row];
 	}
 	
+	/// Append a row in the table, transferring ownership of this row to the table
+	/// and returning a mutable reference to the row
 	pub fn add_row(&mut self, row: Vec<String>) -> Result<&mut Vec<String>, &str> {
 		if row.len() != self.num_cols {
 			return Err("Row does not have the proper number of column");
@@ -39,11 +50,13 @@ impl Table {
 		return Ok(self.get_mut_row(l));
 	}
 	
+	/// Append an empty row in the table. Return a mutable reference to this new row.
 	pub fn add_empty_row(&mut self) -> Result<&mut Vec<String>, &str> {
 		let n = self.num_cols;
 		return Ok(try!(self.add_row(vec!["".to_string(); n])));	
 	}
 	
+	/// Modify a single element in the table
 	pub fn set_element(&mut self, element: String, column: usize, row: usize) -> Result<(), &str> {
 		if column >= self.num_cols {
 			return Err("Column index is higher than expected");
@@ -59,6 +72,7 @@ impl Table {
 		return Ok(());
 	}
 	
+	/// Remove a row
 	pub fn remove_row(&mut self, row: usize) {
 		self.rows.remove(row);
 	}
@@ -77,40 +91,51 @@ impl Table {
 		return Ok(width);
 	}
 	
-	fn print_line_separator(&self, col_width: &[usize]) {
-		print!("+");
+	fn print_line_separator(&self, out: &mut Write, col_width: &[usize]) -> Result<(), Error> {
+		try!(write!(out, "+"));
 		for i in 0..self.num_cols {
 			for _ in 0..(col_width[i] + 2) {
-				print!("-");
+				try!(write!(out, "-"));
 			}
-			print!("+");
+			try!(write!(out, "+"));
 		}
-		println!("");
+		return writeln!(out, "");
 	}
 	
-	fn print_line(&self, line: &[String], col_width: &[usize]) {
-		print!("|");
+	fn print_line(&self, out: &mut Write, line: &[String], col_width: &[usize]) -> Result<(), Error> {
+		try!(write!(out, "|"));
 		for i in 0..self.num_cols {
-			print!(" {} ", line[i]);
+			try!(write!(out, " {} ", line[i]));
 			for _ in 0..(col_width[i] - line[i].len()) {
-				print!(" ");
+				try!(write!(out, " "));
 			}
-			print!("|");
+			try!(write!(out, "|"));
 		}
-		println!("");
+		return writeln!(out, "");
 	}
 	
-	pub fn print(&self) {
+	/// Print the table to `out`
+	pub fn print(&self, out: &mut Write) -> Result<(), Error> {
 		let mut col_width = vec![0usize; self.num_cols];
 		for i in 0..self.num_cols {
 			col_width[i] = self.get_col_width(i).unwrap();
 		}
-		self.print_line_separator(&col_width);
-		self.print_line(&self.titles, &col_width);
-		self.print_line_separator(&col_width);
+		try!(self.print_line_separator(out, &col_width));
+		try!(self.print_line(out, &self.titles, &col_width));
+		try!(self.print_line_separator(out, &col_width));
 		for r in &self.rows {
-			self.print_line(r, &col_width);
-			self.print_line_separator(&col_width);
+			try!(self.print_line(out, r, &col_width));
+			try!(self.print_line_separator(out, &col_width));
 		}
+		return out.flush();
+	}
+	
+	/// Print the table to standard output
+	/// # Panic
+	/// Panic if writing to standard output fails
+	pub fn printstd(&self) {
+		self.print(&mut stdout())
+			.ok()
+			.expect("Cannot print table to standard output");
 	}
 }
